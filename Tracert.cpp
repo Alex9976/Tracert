33c94532e22c4d8cf41e610d1b06ec551d3d74bd
Александр <alexandr99976@gmail.com>
2021-02-10 22:26:24 +0300

    Добавьте файлы проекта.

---
+++ Tracert.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct ICMP_header
+{
+    unsigned char i_type;
+    unsigned char i_code;
+    unsigned short i_checksum;
+    unsigned short i_id;
+    unsigned short i_num;
+} IcmpHeader;
+
+int main(int argc, char* argv[])
+{
+    if (argc == 1)
+    {
+        cout << "\nUsage: tracert.exe endName\n";
+        return 1;
+    }
+    else if (argc != 2)
+    {
+        cout << "Parameters passed incorrectly\n";
+        return 1;
+    }
+    string ending_adr = argv[1];
+    cout << ending_adr;
+
+}
